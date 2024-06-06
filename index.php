9da29d6c48701413b0605cf5182600ea024c6a7a
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-06-05 18:41:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -82,12 +82,12 @@
     import { getFirestore, doc, setDoc, collection, getDocs, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
     const firebaseConfig = {
-        apiKey: "AIzaSyBfiw-MY5ZfXxzBsx2vomazX4LfbAXqVpc",
-        authDomain: "mosue-3f4e5.firebaseapp.com",
-        projectId: "mosue-3f4e5",
-        storageBucket: "mosue-3f4e5.appspot.com",
-        messagingSenderId: "264425567241",
-        appId: "1:264425567241:web:44d858d05e966d5459f4d8"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
     };
 
     const app = initializeApp(firebaseConfig);
